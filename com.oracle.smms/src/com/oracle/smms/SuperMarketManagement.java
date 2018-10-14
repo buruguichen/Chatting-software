d446e1a3b8131737e391ed856eb1427fe83da93c
sword <sword@192.168.0.109>
2018-10-14 21:26:23 +0800

    这是超市管理系统的代码

--- com.oracle.smms/src/com/oracle/smms/SuperMarketManagement.java
+++ com.oracle.smms/src/com/oracle/smms/SuperMarketManagement.java
@@ -15,7 +15,10 @@ public class SuperMarketManagement {
 	static int[] commodityAmount = new int[50];
 	
 	//这是新添加商品时的加入位置
-	static int pos = 0;
+	static int pos = 4;
+	
+	//仓库存储商品的容量
+	static final int commodityMax = commodityNumber.length;
 	
 	//这是预设的超市管理员账号和密码
 	static String adminUsername = "Ruler";
@@ -25,6 +28,17 @@ public class SuperMarketManagement {
 	static int inputTime = 0;
 	
 	public static void main(String[] args) {
+		
+//		这是预设的七个商品信息
+		commodityNumber[1] = 1;		commodityName[1] = "saber";
+		commodityPrice[1] = 9.9f;	commodityAmount[1] = 1;
+		commodityNumber[2] = 2;		commodityName[2] = "lancer";
+		commodityPrice[2] = 3.6f;	commodityAmount[2] = 78;
+		commodityNumber[3] = 7;		commodityName[3] = "berserker";
+		commodityPrice[3] = 8.7f;	commodityAmount[3] = 8;
+		commodityNumber[0] = 5;		commodityName[0] = "caster";
+		commodityPrice[0] = 4.3f;	commodityAmount[0] = 5;
+		
 		showWelcome();
 	}
 	/**
@@ -118,8 +132,9 @@ public class SuperMarketManagement {
 		System.out.println("2.删除商品");
 		System.out.println("3.修改商品");
 		System.out.println("4.查看商品");
-		System.out.println("5.返回上页");
-		System.out.println("6.退出");
+		System.out.println("5.查找商品");
+		System.out.println("6.返回上页");
+		System.out.println("7.退出");
 		
 		choiceCommodityMenu();
 	}
@@ -151,9 +166,14 @@ public class SuperMarketManagement {
 			}
 			case 5:
 			{
-				showMainMenu();
+				searchCommodity();
+				break;
 			}
 			case 6:
+			{
+				showMainMenu();
+			}
+			case 7:
 			{
 				System.exit(0);
 			}
@@ -169,22 +189,27 @@ public class SuperMarketManagement {
 	
 //	 这是添加商品的方法
 	public static void addCommodity() {
-		inputNumber();
-		inputName();
-		System.out.println("请输入添加商品的价格：");
-		float jg = SystemIn.nextFloat();
-		commodityPrice[pos] = jg;
-		System.out.println("请输入添加商品的数量：");
-		int sl = SystemIn.nextInt();
-		commodityAmount[pos] = sl;
-		
-		System.out.println("商品编号     商品名称     "
-				+ "商品价格     商品数量");
-		System.out.println(commodityNumber[pos]+"        "+commodityName[pos]+"        "
-						+commodityPrice[pos]+"        "+commodityAmount[pos]);
-		
-		//将添加商品的位置后移一位
-		pos ++;
+		if(pos == commodityMax) {
+			System.out.println("仓库存储商品已达容量上限，无法添加商品！");
+		}
+		else {
+			inputNumber();
+			inputName();
+			System.out.println("请输入添加商品的价格：");
+			float jg = SystemIn.nextFloat();
+			commodityPrice[pos] = jg;
+			System.out.println("请输入添加商品的数量：");
+			int sl = SystemIn.nextInt();
+			commodityAmount[pos] = sl;
+			
+			System.out.println("商品编号     商品名称     "
+					+ "商品价格     商品数量");
+			System.out.println(commodityNumber[pos]+"        "+commodityName[pos]+"        "
+							+commodityPrice[pos]+"        "+commodityAmount[pos]);
+			
+			//将添加商品的位置后移一位
+				pos ++;
+		}		
 		commodityChoice();
 	}
 	
@@ -251,14 +276,58 @@ public class SuperMarketManagement {
 	
 //	这是展示商品的方法
 	public static void showCommodity() {
-		System.out.println("商品编号     商品名称     "
-				+ "商品价格     商品数量");
-		for(int i=0;i<pos;i++) {
-			System.out.println(commodityNumber[i]+"        "+commodityName[i]+"        "
-							+commodityPrice[i]+"        "+commodityAmount[i]);
+		if(pos == 0) {
+			System.out.println("仓库中没有商品存放");
 		}
-		
+		else {
+			System.out.println("商品编号     商品名称     "
+					+ "商品价格     商品数量");
+			for(int i=0;i<pos;i++) {
+				System.out.println(commodityNumber[i]+"        "+commodityName[i]+"        "
+								+commodityPrice[i]+"        "+commodityAmount[i]);
+			}
+		}
 		commodityChoice();
+	}
+	
+//  这是查找商品的方法
+	public static void searchCommodity() {
+		
+		if(pos == 0) {
+			System.out.println("仓库中没有商品存放，无法查找！");
+			System.out.println();
+			commodityMenu();
+		}
+		else {
+			System.out.println("1.全名查找");
+			System.out.println("2.价格区间查找");
+			System.out.println("3.模糊查找");
+			System.out.println("4.返回商品管理菜单");
+			System.out.println("5.退出");
+			System.out.println("请输入您的选择：");
+			int bh = SystemIn.nextInt();
+			if(bh == 1) {
+				nameSearch();
+			}
+			else if(bh == 2) {
+				priceRangeSearch();
+			}
+			else if(bh == 3) {
+				fuzzySearch();
+			}
+			else if(bh == 4) {
+				System.out.println();
+				commodityMenu();
+			}
+			else if(bh == 5) {
+				System.exit(0);;
+			}
+			else {
+				System.out.println("您的输入有误。请重新输入！");
+				searchCommodity();
+			}
+			
+		}
 	}
 
 //  这是添加商品时写入商品编号并判断写入的商品编号是否重复的方法
@@ -332,12 +401,8 @@ public class SuperMarketManagement {
 			repeatChoiceUpdate(i);
 		}
 		else if(ChoiceUpdate == 5) {
-			System.out.println("请输入新的编号：");
-			int newBh = SystemIn.nextInt();
-			commodityNumber[i] = newBh;
-			System.out.println("请输入新的名称：");
-			String newMc = SystemIn.nextLine();
-			commodityName[i] = newMc;
+			changeNumber(i);
+			changeName(i);
 			System.out.println("请输入新的价格：");
 			float newJg = SystemIn.nextFloat();
 			commodityPrice[i] = newJg;
@@ -390,6 +455,74 @@ public class SuperMarketManagement {
 				}
 			}	
 	}
+	
+//	这是查找商品功能里的全名查找功能
+	public static void nameSearch() {
+		System.out.println();
+		System.out.println("请输入要查找的商品名：");
+		String spm = SystemIn.nextLine();
+		for(int i=0;i<pos;i++) {
+			if(commodityName[i].equals(spm)) {
+				System.out.println("查询到的商品信息为：");
+				System.out.println();
+				System.out.println("商品编号     商品名称     "
+						+ "商品价格     商品数量");
+				System.out.println(commodityNumber[i]+"        "+commodityName[i]+"        "
+									+commodityPrice[i]+"        "+commodityAmount[i]);
+				break;
+			}
+			else if(i == pos-1) 
+				System.out.println("未查询到该商品。");
+		}
+		repeatChoiceSearch(1);
+	}
+	
+//	这是查找商品功能里的价格区间查找功能
+	public static void priceRangeSearch() {
+		System.out.println();
+		System.out.println("请输入要查找的商品价格区间，价格最小值为：");
+		float jgMin = SystemIn.nextFloat();
+		System.out.println("价格最大值为：");
+		float jgMax = SystemIn.nextFloat();
+		System.out.println("查询到的商品信息为：");
+		System.out.println();
+		System.out.println("商品编号     商品名称     "
+				+ "商品价格     商品数量");
+		for(int i=0;i<pos;i++) {
+			if(commodityPrice[i]<jgMax && commodityPrice[i]>jgMin) {
+				System.out.println(commodityNumber[i]+"        "+commodityName[i]+"        "
+									+commodityPrice[i]+"        "+commodityAmount[i]);
+			}
+			else if(i == pos-1) 
+				System.out.println("未查询到在该价格区间的商品。");
+		}
+		repeatChoiceSearch(2);
+	}
+	
+//	这是查找商品功能里的模糊查找功能
+	public static void fuzzySearch() {
+		System.out.println("请输入字符：");
+		String mhm = SystemIn.nextLine();
+		System.out.println("查询到的商品信息为：");
+		for(int i=0;i<pos;i++) {
+			if(i<pos) {
+				String[] mzzu = commodityName[i].split("");
+				
+				for(int j=0;j<mzzu.length;j++) {
+					if(mzzu[j].equals(mhm)) {
+						System.out.println(commodityNumber[i]+"        "+commodityName[i]+"        "
+											+commodityPrice[i]+"        "+commodityAmount[i]);
+						break;
+					}
+				}
+			}
+			
+//			else if(i == pos-1) 
+//				System.out.println("未查询到字符匹配的商品。");
+		}
+		repeatChoiceSearch(3);
+	}
+	
 //	这是更改完商品属性后选择是否继续更改的功能
 	public static void repeatChoiceUpdate(int i) {
 		System.out.println();
@@ -408,6 +541,40 @@ public class SuperMarketManagement {
 		else {
 			System.out.println("您的输入有误。请重新输入！");
 			repeatChoiceUpdate(i);
+		}
+	}
+
+//	这是每次查询商品后，选择继续查询还是返回商品管理菜单或退出的功能
+	public static void repeatChoiceSearch(int a) {
+		System.out.println();
+		System.out.println("1.继续查找");
+		System.out.println("2.返回商品查询菜单");
+		System.out.println("3.返回商品管理菜单");
+		System.out.println("请选择：");
+		int ChoiceSearch = SystemIn.nextInt();
+		if(ChoiceSearch == 1) {
+			System.out.println();
+			if(a == 1) {
+				nameSearch();
+			}
+			if(a == 2) {
+				priceRangeSearch();
+			}
+			if(a == 3) {
+				fuzzySearch();
+			}
+		}
+		else if(ChoiceSearch == 2) {
+			System.out.println();
+			searchCommodity();
+		}
+		else if(ChoiceSearch == 3) {
+			System.out.println();
+			commodityMenu();
+		}
+		else {
+			System.out.println("您的输入有误。请重新输入！");
+			repeatChoiceSearch(a);
 		}
 	}
 	
